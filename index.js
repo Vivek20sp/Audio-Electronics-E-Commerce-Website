2d5f09bf04fa9718f157a391392e668ff904d1d2
Vivek Shankar Phadake <129990463+Vivek20sp@users.noreply.github.com>
2024-06-05 00:00:50 +0530

    Third Commit

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from 'express';
 import ConnectToDatabase from './db.js';
 import router from './routes/productRoute.js';
 import dotenv from "dotenv";
+import cors from 'cors';
 
 const app = express();
 const port = 4000;
 dotenv.config();
 
-
 ConnectToDatabase();
+
+app.use(cors());
 
 app.use('/api',router);
 
